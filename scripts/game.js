10c1e249cf286d85302e9ae0ca3c5b582af6928f
ellis882 <ellisrachel1304@me.com>
2022-02-28 14:03:54 +0000

    added tests to game

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,8 @@
 let game = {
     currentGame: [],
+    playersMoves: [],
     score: 0,
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -20,4 +20,13 @@ describe("game object contains correct keys", () => {
     test("currentGame key exists", () => {        
         expect("currentGame" in game).toBe(true);
     });
+    test("playersMoves key exists", () => {        
+        expect("playersMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {        
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contains correct id's", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });    
 });
